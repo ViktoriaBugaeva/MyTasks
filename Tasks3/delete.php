9b3ad94a45c270deb4ccb87707035e618d41a8a3
LaineJakutkina <38099059+LaineJakutkina@users.noreply.github.com>
2019-02-12 19:58:43 +0200

    4 update

---
+++ Tasks3/delete.php
@@ -0,0 +1,10 @@
+<?php
+$id = $_GET['id'];
+
+$pdo = new PDO("mysql:host=localhost; dbname=test", "root", "");
+$sql = 'DELETE FROM tasks WHERE id=:id';
+$statement = $pdo->prepare($sql);
+$statement->bindParam(":id", $id);
+$statement->execute();
+
+header("Location: http://localhost/MyTasks/Tasks2/");
